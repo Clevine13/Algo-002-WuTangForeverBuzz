51bd7f5e0f87bd34142c1e292b4643ce1336ca44
Carl Levine Jr <clevine313@gmail.com>
2019-07-30 11:03:51 -0400

    Message

--- src/main/java/hbcu/stay/ready/algorithms/WuTangForeverBuzz.java
+++ src/main/java/hbcu/stay/ready/algorithms/WuTangForeverBuzz.java
@@ -3,6 +3,23 @@ package hbcu.stay.ready.algorithms;
 public class WuTangForeverBuzz {
 
     public String wuTangClan(){
-        return null;
+        int count = 0;
+        String Wutang = "";
+        while(count < 15){
+            count++;
+            if((count %3==0) && (count%5==0)){
+                System.out.println(Wutang += "WuTang Forever");
+            }
+            else if( count%3==0){
+                System.out.println(Wutang += "Wu\n");
+            }
+            else if(count %5==0){
+                System.out.println(Wutang += "Tang\n");
+            }
+            else{
+                System.out.println(Wutang += (count)+"\n");
+            }
+        }
+        return Wutang;
     }
 }
